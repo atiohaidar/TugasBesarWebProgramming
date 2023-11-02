5f2b53dc90efe5596576513c8a2ba7c85a450b6f
Tio Haidar <atiohaidar@gmail.com>
2023-11-02 10:17:50 +0700

    nge nambah layout untuk beberapa tampilan

--- index.js
+++ index.js
@@ -1,24 +1,34 @@
 const express = require('express')
 const app = express()
 const port = 3000
-// const expressLayouts = require('express-ejs-layouts')
+const expressLayouts = require('express-ejs-layouts')
 
 app.set('view engine', 'ejs');
-// app.use(expressLayouts); // ini juga third-party midleware
+app.use(expressLayouts); // ini juga third-party midleware
 
 app.use(express.static('public'));
 
 
-app.get('/login', (req,res)=>{
-    res.render('Login_Register/login_register') 
-})
-
-
 app.get('/', (req,res)=>{
-    res.render('Main_Page/index', {phone_number : '+62 858 1564 8255'}) 
+    res.render('Main_Page/index', {phone_number : '+62 858 1564 8255',
+    title: "Main Page",
+    layout: "layouts/main-layout"
+}) 
 })
+
+app.get('/login', (req,res)=>{
+    res.render('Login_Register/login_register', {
+        title: "Login",
+    layout: "layouts/bs-layout"
+    }) 
+})
+
 app.get('/gallery', (req,res)=>{
-    res.render('Main_Page/Gallery') 
+    res.render('Main_Page/Gallery',{
+        title: "Galery Page",
+    layout: "layouts/main-layout",
+    phone_number : '+62 858 1564 8255'
+    }) 
 })
 
 
