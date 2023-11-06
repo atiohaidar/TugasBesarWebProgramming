1cf8e39f5289262199f60da5d3556ef43118892a
Rzv888 <127009592+Rzv888@users.noreply.github.com>
2023-11-06 02:12:03 +0000

    bener

--- public/js/Profile.js
+++ public/js/Profile.js
@@ -4,20 +4,20 @@ const openLogoutModalLink = document.querySelector('#openLogoutModal');
 const cancelButton = document.querySelector('.cancelButton');
 const yesButton = document.querySelector('.yesButton');
 
-// Fungsi untuk menampilkan modal
+// Fungsi untuk menampilkan popup
 function openLogoutModal() {
     logoutModal.style.display = 'block';
 }
 
-// Fungsi untuk menyembunyikan modal
+// Fungsi untuk menyembunyikan popup
 function closeLogoutModal() {
     console.log("hallo")
     logoutModal.style.display = 'none';
 }
 
-// Tambahkan event listener untuk menampilkan modal saat tautan "Logout" ditekan
+// Tambahkan event listener untuk menampilkan popup saat tautan "Logout" ditekan
 openLogoutModalLink.addEventListener('click', openLogoutModal);
 
-// Tambahkan event listener untuk menyembunyikan modal saat tombol "Cancel" atau "YES" ditekan
+// Tambahkan event listener untuk menyembunyikan popup saat tombol "Cancel" atau "YES" ditekan
 cancelButton.addEventListener('click', closeLogoutModal);
 yesButton.addEventListener('click', closeLogoutModal);
